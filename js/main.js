d0d74d3ae432d40f70b9c4bf1113d74512089488
Constantine Kobushka <kobushkaconstantine@gmail.com>
2024-10-11 17:38:53 +0300

    fixed mobile menu

--- js/main.js
+++ js/main.js
@@ -1,32 +1,68 @@
-const burgerBtn = document.querySelector('.burger-btn');
-const mobileCloseBtn = document.querySelector('.mobile-close-btn');
-const mobileNav = document.querySelector('.mobile-nav');
-const ordeBtns = document.querySelectorAll('.order-btn');
-const backdrop = document.querySelector('.backdrop');
-const modal = document.querySelector('.modal');
-const modalCloseBtn = document.querySelector('.modal-close-btn');
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerBtn = document.querySelector('.burger-btn');
+  const mobileCloseBtn = document.querySelector('.mobile-close-btn');
+  const mobileOverlay = document.querySelector('.mobile-overlay');
+  const mobileNav = document.querySelector('.mobile-nav');
 
-function openMobileMenuHandler() {
-  mobileNav.classList.add('is-open');
-}
+  const orderBtns = document.querySelectorAll('.order-btn');
+  const backdrop = document.querySelector('.backdrop');
+  const modal = document.querySelector('.modal');
+  const modalCloseBtn = document.querySelector('.modal-close-btn');
 
-function closeMomobileMenuHandler() {
-  mobileNav.classList.remove('is-open');
-}
+  function openMobileMenuHandler() {
+    mobileOverlay.classList.add('is-open');
+    mobileNav.classList.add('is-open');
+  }
 
-function openModalHandler() {
-  backdrop.classList.add('is-open');
-}
+  function closeMobileMenuHandler() {
+    mobileOverlay.classList.remove('is-open');
+    mobileNav.classList.remove('is-open');
+  }
 
-function closeModalHandler() {
-  backdrop.classList.remove('is-open');
-}
+  function openModalHandler() {
+    backdrop.classList.add('is-open');
+  }
 
-burgerBtn.addEventListener('click', openMobileMenuHandler);
-mobileCloseBtn.addEventListener('click', closeMomobileMenuHandler);
-ordeBtns.forEach((orderBtn) => orderBtn.addEventListener('click', openModalHandler));
-modalCloseBtn.addEventListener('click', closeModalHandler);
-backdrop.addEventListener('click', closeModalHandler);
-modal.addEventListener('click', function (event) {
-  event.stopPropagation();
+  function closeModalHandler() {
+    backdrop.classList.remove('is-open');
+  }
+
+  function stopPropagation(e) {
+    e.stopPropagation();
+  }
+
+  function scrollToSection(event) {
+    if (event.target.classList.contains('mobile-nav--link')) {
+      event.preventDefault();
+
+      const targetId = event.target.getAttribute('href');
+
+      if (targetId && targetId.startsWith('#')) {
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+          const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+
+          window.scrollTo({
+            top: targetPosition - 80,
+            behavior: 'smooth',
+          });
+        }
+      }
+
+      closeMobileMenuHandler();
+    }
+  }
+
+  burgerBtn.addEventListener('click', openMobileMenuHandler);
+  mobileCloseBtn.addEventListener('click', closeMobileMenuHandler);
+  mobileOverlay.addEventListener('click', closeMobileMenuHandler);
+  mobileNav.addEventListener('click', stopPropagation);
+  mobileNav.addEventListener('click', scrollToSection);
+
+  orderBtns.forEach((orderBtn) => orderBtn.addEventListener('click', openModalHandler));
+  modalCloseBtn.addEventListener('click', closeModalHandler);
+  backdrop.addEventListener('click', closeModalHandler);
+
+  modal.addEventListener('click', stopPropagation);
 });
